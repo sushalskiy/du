665a22850d2045bd9d2a52f91bcb1fd2ce5b96dd
ihormihal <imykhalchenko@jayadigital.com>
2015-04-10 15:35:08 +0300

    analyze page

--- js/init.js
+++ js/init.js
@@ -4,5 +4,32 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+    //header gap
+    if($('#navigation').length > 0){
+    	$('main').css('margin-top',$('#navigation').height());
+    }
+    
+    //on scroll
+    $(window).scroll(function(){
+    	if($(window).scrollTop() > 150){
+    		$('#navigation-home').removeClass('transparent');
+    	}else{
+    		$('#navigation-home').addClass('transparent');
+    	}
+    });
+
+    //on windo resize
+    $(window).resize(function(){
+    	headerGap();
+    });
+  	
+
   }); // end of document ready
+
+  var headerGap = function(){
+  	if($('#navigation').length > 0){
+    	$('main').css('margin-top',$('#navigation').height());
+    }
+  }
+
 })(jQuery); // end of jQuery name space
