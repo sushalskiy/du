848637de24dbc7dc612260adf444f03b871977fc
ihormihal <imykhalchenko@jayadigital.com>
2015-04-15 19:54:45 +0300

    reports

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 require 'compass/import-once/activate'
+require 'font-awesome-sass'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
--- js/init.js
+++ js/init.js
@@ -8,6 +8,8 @@
     if($('#navigation').length > 0){
     	$('main').css('margin-top',$('#navigation').height());
     }
+
+    //$('.materialboxed').materialbox();
     
     //on scroll
     $(window).scroll(function(){
@@ -21,6 +23,20 @@
     //on windo resize
     $(window).resize(function(){
     	headerGap();
+    });
+
+    $('.drop-button').click(function(){
+      var visible_class = 'mdi-navigation-expand-less'
+      var hidden_class = 'mdi-navigation-expand-more'
+      var container = $($(this).data('drop'));
+      var icon = $(this).find('i');
+      if(container.is(':visible')){
+        container.slideUp(250);
+        icon.attr('class',hidden_class);
+      }else{
+        container.slideDown(250);
+        icon.attr('class',visible_class);
+      }
     });
   	
 
